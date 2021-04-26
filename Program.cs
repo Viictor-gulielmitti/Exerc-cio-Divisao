8463f28a45cb7e08f86fa5b06c39cabfb24aed11
Viictor-gulielmitti <Victorgulielmitti284@gmail.com>
2021-04-25 21:43:06 -0300

    divisão

---
+++ Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace Exerc_cio_Divisao
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+         double Numerador, Denominador, resultado; 
+
+         
+           
+        Console.WriteLine("digite um Numerador");
+        Numerador = Convert.ToDouble(Console.ReadLine()); 
+
+        Console.WriteLine("digite um Denominador");
+        Denominador = Convert.ToDouble(Console.ReadLine()); 
+
+        resultado = Numerador / Denominador;
+
+
+         if ( Denominador == 0){
+        Console.WriteLine("impossivel dividir por zero"); 
+         }
+         else {
+        Console.WriteLine ($" { Numerador } dividido por { Denominador } é { resultado }");
+        }
+
+        
+        
+
+        
+
+
+
+
+
+        
+
+
+        }
+    }
+}
